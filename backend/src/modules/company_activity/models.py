0b36cf771a6fdfcc8e0bbe6015125db088bb606c
Alireza Sarabadani <77675736+srbadni@users.noreply.github.com>
2026-08-24 17:42:33 +0330

    fix SQLAlchemy model type hints

--- backend/src/modules/company_activity/models.py
+++ backend/src/modules/company_activity/models.py
@@ -15,4 +15,4 @@ class CompanyActivity(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(String, nullable=False)
 
-    companies: Mapped["Company"] = relationship("Company", back_populates="activity", init=False)
+    companies: Mapped[list["Company"]] = relationship("Company", back_populates="activity", init=False)
