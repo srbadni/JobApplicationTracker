20c40ce5dd40790bd924ace1847a24e24de04f06
alireza sarabadani <srbadni@gmail.com>
2026-08-24 17:11:15 +0330

    add general information to applicant_profile

--- backend/src/modules/applicant_profile/models.py
+++ backend/src/modules/applicant_profile/models.py
@@ -1,8 +1,10 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey, Integer
+from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from .enums import Gender, MartialStatus
+from ..job_posting.enums import MilitaryServiceStatus
 from ...infrastructure.database import Base
 from ...infrastructure.database.models import TimestampMixin
 
@@ -20,5 +22,14 @@ class ApplicantProfile(Base, TimestampMixin):
         nullable=False,
         unique=True,
     )
+
+    specialization: Mapped[str | None] = mapped_column(String(100), init=False)
+    birth_year: Mapped[int | None] = mapped_column(Integer, init=False)
+    gender: Mapped[Gender | None] = mapped_column(String(20), init=False)
+    military_status: Mapped[MilitaryServiceStatus | None] = mapped_column(String(50), init=False)
+    martial_status: Mapped[MartialStatus | None] = mapped_column(String(20), init=False)
+    province: Mapped[str | None] = mapped_column(String(50), init=False)
+    address: Mapped[str | None] = mapped_column(String, init=False)
+    about: Mapped[str | None] = mapped_column(String, init=False)
 
     applicant: Mapped["User"] = relationship("User", back_populates="applicant_profile", init=False)
